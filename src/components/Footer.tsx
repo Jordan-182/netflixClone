60603fce630d5215a0591cd55a0f0c0dae0f139e
Jordan-182 <jordan.p62@outlook.fr>
2025-04-04 09:35:08 +0200

    logo path fixed on header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
+import logo from "../assets/img/wildflixLogo.png";
 import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="../src/assets/img/wildflixLogo.png" alt="logo wildflix" />
+        <img src={logo} alt="logo wildflix" />
         <p>Tous droits réservés - Wildflix 2025</p>
       </footer>
     </>
